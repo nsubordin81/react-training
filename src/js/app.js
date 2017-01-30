bfd52a659176fb58cb10ac6fc96b8f71f4901196
Taylor Bird <nsubordin81@gmail.com>
2017-01-30 13:23:14 -0500

    first crack, using non dynamic table rows

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import 'bootstrap-loader';
-import '../css/styles.scss';
+const colors = ['green', 'yellow','black','red','white','blue']
 
-class HelloWorld extends React.Component {
-
+class ColorList extends React.Component {
+    
+    
     render() {
-        return <h1>Hello World!</h1>;
+        //return React.createElement('h1', null, 'Color List')
+
+        return (
+            <div> 
+                <h1>Color List</h1>
+                <ul>
+                    {
+                        this.props.myColors.map(myColor => 
+                        <li>{myColor}</li>
+                    )}
+                </ul>
+            </div>
+        );
     }
 }
 
-ReactDOM.render( <HelloWorld /> , document.querySelector('main'));
+const cars = [{make: 'toyota', model: 'camry', year: 2015, color: 'black', price: '$10,000'}, {make: 'honda', model: 'civic', year: 2009, color: 'white', price: '$9,000'}, {make: 'ford', model: 'focus', year: 2000, color: 'red', price: '$5,000'}]
+
+class TaylorsCarEmporium extends React.Component {
+    
+    thead() {
+        return (
+            <thead>
+                <tr>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Year</th>
+                    <th>Color</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+        );
+    }
+
+    tbody() {
+        return (
+            <tbody>
+                {
+                    this.props.myCars.map(myCar =>
+                    <tr>
+                        <td>{myCar.make}</td>
+                        <td>{myCar.model}</td>
+                        <td>{myCar.year}</td>
+                        <td>{myCar.color}</td>
+                        <td>{myCar.price}</td>
+                    </tr>
+                )}
+            </tbody>
+        );
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Taylor's Car Emporium</h1>                
+                <table>
+                  {this.thead()}
+                  {this.tbody()}   
+                </table>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<TaylorsCarEmporium myCars={cars}/>, document.querySelector('main'))
+// ReactDOM.render(<ColorList myColors={colors} />, document.querySelector('main'))
