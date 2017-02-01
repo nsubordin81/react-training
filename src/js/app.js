3e2447f54f68f4c5200af2376bca24ff26372166
Taylor Bird <nsubordin81@gmail.com>
2017-02-01 15:44:00 -0500

    omg it worked! that was redux implemented with my small component tree

--- src/js/app.js
+++ src/js/app.js
@@ -1,67 +1,83 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { connect } from 'react-redux';
+import {createStore} from 'redux';
+import {connect} from 'react-redux';
 
+const items = ['Raindrops', 'roses', 'whiskers', 'kittens'];
 
-const createAddAction = value => ({
+class MyFavList extends React.Component {
+    render() {
+        return (
+            <UpdateableList myItems={this.props.items} addToList={this.props.addToList} />
+        )
+    }
+}
+
+class ListItem extends React.Component {
+    render() {
+        return (
+            <li>{this.props.item}</li>
+        )
+    }
+}
+
+class UpdateableList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            favoriteItem: ''
+        }
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.currentTarget.name]: e.currentTarget.value
+        });
+    };
+
+    addItem = () => {
+        this.props.addToList(this.state.favoriteItem)
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.props.myItems.map( item =>
+                        <ListItem item={item} />
+                    )}
+                </ul>
+                <input type='text' id='favorite-item-input' name='favoriteItem' value={this.state.inputValue} onChange={this.onChange}></input>
+                <button onClick={this.addItem}>Add Item</button>
+            </div>
+        )
+    }
+}
+
+const addItem = value => ({
     type: 'ADD',
     value
 })
 
-const createSubtractAction = value => ({
-    type: 'SUBTRACT',
-    value
-})
-
-const reducer = (state = 0, action) => {
+const reducer = (state = items, action) => {
 
     if(action == null) return state; 
 
     console.log('state: ', state, 'action: ', action);
         switch(action.type) {
             case 'ADD':
-                return state + action.value;
-            case 'SUBTRACT':
-                return state - action.value;
+                return state.concat(action.value);
             default:
                 return state;
         }
-}
-
-
-// const createStore = reducer => {
-//     let currentState;
-//     const fns = [];
-//     return {
-//         getState: () => currentState,
-//         dispatch: action => {
-//             currentState = reducer(currentState, action);
-//             fns.forEach(fn => fn());
-//         },
-//         subscribe: fn => fns.push(fn),
-//     };
-// };
-
-const appStore = createStore(reducer);
-
-class Calculator extends React.Component {
-    render() {
-        return <div>
-            <div>Current Value: {this.props.currentValue}</div>
-            <button onClick={this.props.addFive}>Add 5 </button>
-            <button onClick={this.props.subtractFive}>Subtract 5</button>
-        </div>
-    }
-    
 }
 
 const mapStateToProps = appState => {
 
     //props passed into the Component
     return {
-        currentValue: appState
+        items: appState
     }
 
 }
@@ -69,43 +85,15 @@ const mapStateToProps = appState => {
 const mapDispatchToProps = dispatch => {
     //props passed into the component
     return {
-        addFive: dispatch(createAddAction(5)),
-        subtractFive: dispatch(createSubtractAction(5))
+        addToList: (item) => dispatch(addItem(item)),
     }
 };
 
 
-// const connect = (mapStateToProps, mapDispatchToProps) => {
-//     return (componentToWrap) => {
-//         return class Container extends React.Component {
-//             static propTypes = {
-//                 store: React.PropTypes.object
-//             };
-//             componentDidMount() {
-//                 this.props.store.subscribe(() => {
-//                     this.forceUpdate();
-//                 });
-//                 this.props.store.dispatch({ type: 'NOOP' });
-//             }
-//             render() {
-//                 const componentProps = {};
-//                 Object.assign(componentProps, mapStateToProps(this.props.store.getState()));
-//                 Object.assign(componentProps, mapDispatchToProps(this.props.store.dispatch));
-//                 return React.createElement(componentToWrap, componentProps);
-//             }
-//         };
-//     };
-// };
 
 
-const CalculatorContainer = connect(mapStateToProps, mapDispatchToProps)(Calculator);
+const appStore = createStore(reducer);
 
+const MyFavListContainer = connect(mapStateToProps, mapDispatchToProps)(MyFavList);
 
-
-
-
-ReactDOM.render(<Calculator store={appStore} />, document.querySelector('main'))
-
-
-console.log('this works');
-// ReactDOM.render(<ColorTool myColors={colors} />, document.querySelector('main'))
+ReactDOM.render(<MyFavListContainer store={appStore} />, document.querySelector('main'));
