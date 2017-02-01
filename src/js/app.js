947542f17bfd263cffb0704cc15b7e763914ece5
Taylor Bird <nsubordin81@gmail.com>
2017-02-01 11:54:22 -0500

    example of what reducer is doing

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
+const result = [
+        {type: 'Add', value: 1},
+        {type: 'Subtract', value: 2},
+        {type: 'Add', value: 3},
+        {type: 'subtract', value: 4},
+        {type: 'Add', value: 5}
+    ].reduce((state, action) => {
+    console.log('state: ', state, 'action: ', action);
+        switch(action.type) {
+            case 'Add':
+                return state + action.value;
+            case 'Subtract':
+                return state - action.value;
+        }
+}, 0);
+
 console.log('this works');
 // ReactDOM.render(<ColorTool myColors={colors} />, document.querySelector('main'))
